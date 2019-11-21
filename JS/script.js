02320b4bbafed0256a63d6fe82d8301e7f577b84
Kervintz Noel <10268341+kervcode@users.noreply.github.com>
2019-11-21 11:49:48 -0500

    Linked jquery

---
+++ JS/script.js
@@ -0,0 +1,7 @@
+// set the focus on the first text field
+$(window).on('load', () => {
+    $('#name').focus();
+})
+
+// reveal a text field when other option is selected in job role field
+const jobRole = $('#title');
