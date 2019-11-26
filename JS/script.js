6a49c9fbf89a144bdd45a038c00190925b2f40cc
Kervintz Noel <10268341+kervcode@users.noreply.github.com>
2019-11-25 20:01:15 -0500

    show input field when other in selected as Job Role

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,26 @@
+//GLOBAL VARAIBLES
+const other = $('#other-title');
+const title = $('#title')
+const jobTitle = $('#title option');
+const input = $('#other-title');
+
+
 // set the focus on the first text field
 $(window).on('load', () => {
     $('#name').focus();
 })
 
+//initially hide text field for input
+input.hide();
+
 // reveal a text field when other option is selected in job role field
-const jobRole = $('#title');
+title.on('change', function (){
+    if(this.value === 'other') {
+        input.delay(100)
+        .slideDown()
+        // .show();
+    }
+})
+
+
+
