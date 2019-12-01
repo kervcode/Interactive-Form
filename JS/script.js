66b3a7ba338192a6c90841a2d7d77ddcd724b3ac
Kervcodes-desktop <Kervintz84@hotmail.com>
2019-12-01 16:56:56 -0500

    Still stock.

--- JS/script.js
+++ JS/script.js
@@ -1,53 +1,73 @@
 //GLOBAL VARAIBLES
-const other = $('#other-title');
-const title = $('#title')
-const jobTitle = $('#title option');
-const input = $('#other-title');
-const selectDesign = $('#design'); 
-const design = $('#design option');
-const tshirtColor = $('#color');
-const tshirtOptions = $('#color option');
+const other = $("#other-title");
+const title = $("#title");
+const jobTitle = $("#title option");
+const input = $("#other-title");
+const selectDesign = $("#design");
+const design = $("#design option");
+const tshirtColor = $("#color");
+const tshirtOptions = $("#color option");
 
-
-// set the focus on the first text field
-$(window).on('load', () => {
-    $('#name').focus();
-})
-
+/**
+ * ------------------Input Feild Section--------------------------
+ * set the focus on the first text field
+ **/
+$(window).on("load", () => {
+  $("#name").focus();
+});
+/**
+ * ------------------Job Role Section--------------------------
+ * set the focus on the first text field
+ **/
 //initially hide text field for input
 input.hide();
 
 // reveal a text field when other option is selected in job role field
-title.on('change', function (){ //why this does not work with arrow function
-    if(this.value === 'other') {
-        input.delay(100)
-        .slideDown()
-        // .show();
-    } else {
-        input.slideUp(150);
+title.on("change", function() {
+  //why this does not work with arrow function
+  if (this.value === "other") {
+    input.delay(100).slideDown();
+    // .show();
+  } else {
+    input.slideUp(150);
+  }
+});
+/**
+ * ----------- T-Shirt Infor Section--------------------------
+ * set the focus on the first text field
+ **/
+design.eq(0).hide();
+
+// Add
+const tshirtTheme = tshirtColor.prepend(
+  "<option>Please select a T-shirt theme</option>"
+);
+
+// tshirtOptions.eq(0).remove("selected", "");
+// tshirtOptions.eq(0).attr("selected", "selected");
+console.log(tshirtOptions);
+
+// Hide the color in the dropdown color field
+tshirtColor.each(function() {
+  tshirtOptions.hide();
+  // .attr("disabled", true)
+  // .attr("hidden", true)
+  // .removeAttr("selected", "");
+});
+
+tshirtTheme.attr("selected", "selected");
+
+selectDesign.on("change", function(event) {
+  console.log($(event.target).val()); //can't call nodeName() - Why?
+  tshirtOptions.each(function(i, element) {
+    if ($(element).val() === "js puns") {
+      //   console.log($(element).val());
+      tshirtOptions
+        .eq(0)
+        .removeAttr("disabled")
+        .show();
+      tshirtOptions.eq(1).show();
+      tshirtOptions.eq(2).show();
     }
-})
-
-//t-shirt section
- design.eq(0).hide();
-//  tshirtColor.hide();
-// console.log(tshirtColor.children().last())
-// console.log(tshirtOptions.eq(0))
-
-// tshirtColor.insertBefore(tshirtOptions.eq(0), "<option>Please select a T-shirt theme</option>")
-
-// tshirtColor.each(function(index, element){
-//     tshirtOptions.hide();
-// })
-
-// tshirtColor.prepend("<option>Please select a T-shirt theme</option>");
-
-//  selectDesign.change(function(event){
-//      console.log(event.target.nodeName)
-//  })
-
-tshirtColor.on('change', function(){
-    
-})
-
-
+  });
+});
