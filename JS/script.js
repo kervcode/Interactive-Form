944a95543c706aca57e6f13893771f7841400efd
Kervcodes-desktop <Kervintz84@hotmail.com>
2019-12-01 18:39:58 -0500

    Hide all color themes - done

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,13 @@
 $(function() {
   //GLOBAL VARAIBLES
-  const other = $("#other-title");
-  const title = $("#title");
-  const jobTitle = $("#title option");
-  const input = $("#other-title");
-  const selectDesign = $("#design");
-  const design = $("#design option");
-  const tshirtColor = $("#color");
-  const tshirtOptions = $("#color option");
+  const $other = $("#other-title");
+  const $title = $("#title");
+  const $jobTitle = $("#title option");
+  const $input = $("#other-title");
+  const $selectDesign = $("#design");
+  const $design = $("#design option");
+  const $tshirtColor = $("#color");
+  const $tshirtOptions = $("#color option");
 
   /**
    * ------------------Input Feild Section--------------------------
@@ -21,54 +21,60 @@ $(function() {
    * set the focus on the first text field
    **/
   //initially hide text field for input
-  input.hide();
+  $input.hide();
 
   // reveal a text field when other option is selected in job role field
-  title.on("change", function() {
+  $title.on("change", function() {
     //why this does not work with arrow function
     if (this.value === "other") {
-      input.delay(100).slideDown();
+      $input.delay(100).slideDown();
       // .show();
     } else {
-      input.slideUp(150);
+      $input.slideUp(150);
     }
   });
   /**
    * ----------- T-Shirt Infor Section--------------------------
    * set the focus on the first text field
    **/
-  design.eq(0).hide();
+  $design.eq(0).hide();
 
-  // Add
-  const tshirtTheme = tshirtColor.prepend(
-    "<option>Please select a T-shirt theme</option>"
-  );
-
-  // tshirtOptions.eq(0).remove("selected", "");
-  // tshirtOptions.eq(0).attr("selected", "selected");
-  console.log(tshirtOptions);
-
-  // Hide the color in the dropdown color field
-  tshirtColor.each(function() {
-    tshirtOptions.hide();
-    // .attr("disabled", true)
-    // .attr("hidden", true)
-    // .removeAttr("selected", "");
+  // Hide all color themes
+  $tshirtOptions.each(function(i, element) {
+    // console.log(tshirtOptions.eq(i).val());
+    if (
+      $(element)
+        .eq(i)
+        .val() !== "cornflowerblue"
+    ) {
+      $(this)
+        // .eq(i) //returned undefined when left uncommented
+        .attr("disabled", true)
+        .attr("hidden", true);
+      console.log($(this));
+    } else {
+      $(this)
+        .before("<option>Please select a T-shirt theme</option>")
+        .attr("selected", "selected");
+      $("#color option:selected")
+        .removeAttr("selected")
+        .attr("hidden", true);
+    }
   });
 
-  tshirtTheme.attr("selected", "selected");
+  // tshirtTheme.attr("selected", "selected");
 
-  selectDesign.on("change", function(event) {
+  $selectDesign.on("change", function(event) {
     console.log($(event.target).val()); //can't call nodeName() - Why?
-    tshirtOptions.each(function(i, element) {
+    $tshirtOptions.each(function(i, element) {
       if ($(element).val() === "js puns") {
         //   console.log($(element).val());
         tshirtOptions
           .eq(0)
           .removeAttr("disabled")
           .show();
-        tshirtOptions.eq(1).show();
-        tshirtOptions.eq(2).show();
+        $tshirtOptions.eq(1).show();
+        $tshirtOptions.eq(2).show();
       }
     });
   });
