363990d09865ca216a5cc7f0628b427b0f567a7b
Kervcodes-desktop <Kervintz84@hotmail.com>
2019-12-15 19:40:36 -0500

    work in progress

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,4 @@
-$(function () {
+$(function() {
   //GLOBAL VARAIBLES
   const $title = $("#title"),
     $input = $("#other-title"),
@@ -8,10 +8,9 @@ $(function () {
 
   let $checkboxInputs = $(".activities"), //store all input checkboxes
     conferencePrice = 0,
-    totalActivityCost = $checkboxInputs.append('<p>Total: $0</p>'),
-    total = $('.activities p');
+    totalActivityCost = $checkboxInputs.append("<p>Total: $0</p>"),
+    total = $(".activities p");
   const $checkboxes = $checkboxInputs.children().children();
-  console.log(total);
   /** Input Feild Section -- set the focus on the first text field **/
   $(window).on("load", () => {
     $("#name").focus();
@@ -22,7 +21,7 @@ $(function () {
   $input.hide();
 
   // reveal a text field when other option is selected in job role field
-  $title.on("change", function () {
+  $title.on("change", function() {
     //why this does not work with arrow function
     if (this.value === "other") {
       $input.delay(100).slideDown();
@@ -35,11 +34,11 @@ $(function () {
   $design.eq(0).hide();
 
   // Hide all color themes
-  $tshirtOptions.each(function (i, element) {
+  $tshirtOptions.each(function(i, element) {
     if (
       $(element)
-      .eq(i)
-      .val() !== "cornflowerblue"
+        .eq(i)
+        .val() !== "cornflowerblue"
     ) {
       $(this)
         // .eq(i) //returned undefined when left uncommented
@@ -55,12 +54,11 @@ $(function () {
     }
   });
 
-
   // show appropriate color
   // for selected theme
-  $selectDesign.on("change", function (event) {
+  $selectDesign.on("change", function(event) {
     console.log($(event.target).val()); //can't call nodeName() - Why?
-    $tshirtOptions.each(function (i, element) {
+    $tshirtOptions.each(function(i, element) {
       if ($(event.target).val() === "js puns") {
         $tshirtOptions
           .slice(0, 3)
@@ -86,67 +84,98 @@ $(function () {
   // Register for activities section
 
   //listening for change in activities
-  $checkboxes.on("change", function (event) {
+  $checkboxes.on("change", function(event) {
     let clicked = event.target;
     let clickedCost = $(clicked).attr("data-cost");
-    let clickedTime = $(clicked).attr('data-day-and-time')
+    let clickedTime = $(clicked).attr("data-day-and-time");
     clickedCost = clickedCost.slice(1, 4);
 
     // calculate the conference price
-    if ($(clicked).prop('checked')) {
+    if ($(clicked).prop("checked")) {
       conferencePrice += parseInt(clickedCost);
     } else {
-      conferencePrice -= parseInt(clickedCost)
+      conferencePrice -= parseInt(clickedCost);
     }
     //append conference price to the page
-    $(total).text('Total  $:' + conferencePrice);
+    $(total).text("Total  $:" + conferencePrice);
     // console.log(conferencePriceDOM.last().text())
-    console.log(' $' + conferencePrice)
+    console.log(" $" + conferencePrice);
 
     for (let i = 0; i < $checkboxes.length; i++) {
-      let checkboxTime = $($checkboxes[i]).attr('data-day-and-time');
+      let checkboxTime = $($checkboxes[i]).attr("data-day-and-time");
       if (clickedTime === checkboxTime && clicked !== $checkboxes[i]) {
-        if ($(clicked).prop('checked')) {
-          $($checkboxes[i]).attr('disabled', true);
+        if ($(clicked).prop("checked")) {
+          $($checkboxes[i]).attr("disabled", true);
         } else {
-          $($checkboxes[i]).attr('disabled', false);
+          $($checkboxes[i]).attr("disabled", false);
         }
       }
     }
   });
   // Form validation Section
-  const $payment = $('#payment');
-  const paymentOption = $payment.children();
-  const $creditCardInfo = $('#credit-card')
+  const form = $("form");
+  const payment = $("#payment");
+  const paymentOption = payment.children();
+  const creditCardInfo = $("#credit-card");
+  const nameInput = $("#name");
+  const emailInput = $("#mail");
+  const activityInput = $(".activities");
+  const creditCardNumber = $("#cc-num");
+  const zipCode = $("#zip");
+  const cvv = $("#cvv");
 
-
-  paymentOption.each(function (i, option) {
-    if (option.value === 'select method') {
-      $(option).attr('disabled', true).attr('hidden', true).removeAttr('selected', '');
+  paymentOption.each(function(i, option) {
+    if (option.value === "select method") {
+      $(option)
+        .attr("disabled", true)
+        .attr("hidden", true)
+        .removeAttr("selected", "");
     }
-    if (option.value === 'Credit Card') {
-      $(option).attr('selected', 'selected');
+    if (option.value === "Credit Card") {
+      $(option).attr("selected", "selected");
     }
+  });
 
-  })
-
-  $payment.on('change', function (e) {
+  payment.on("change", function(e) {
     console.log(e.target.value);
-    if ((e.target.value === 'PayPal') || (e.target.value === 'Bitcoin')) {
-      $creditCardInfo.delay(150).slideUp();
-    } else if (e.target.value === 'Credit Card') {
-      $creditCardInfo.delay(150).slideDown();
+    if (e.target.value === "PayPal" || e.target.value === "Bitcoin") {
+      creditCardInfo.delay(150).slideUp();
+    } else if (e.target.value === "Credit Card") {
+      creditCardInfo.delay(150).slideDown();
     }
-  })
-  console.log(paymentOption)
+  });
+  // console.log(nameInput);
 
-  const nameInput = $('#name');
-  const emailInput = $('#mail');
-  const activityInput = $('.activities');
-  const creditCardNumber = $('#payment option[value = "Credit Card"]');
-  const zipCode = $('#zip');
-  const cvv = $('#cvv')
+  function isValidName(nameInput) {
+    return /^[a-z]+$/.test(nameInput);
+  }
 
-  function isValidName() {}
+  function isValidEmail(emailInput) {
+    return /^[^@]+@[^@]+\.[a-z]+$/i.test(emailInput);
+  }
 
+  function formatCreditCard(creditCardNumber) {
+    return;
+  }
+
+  // function showOrHideTip(show, element) {
+  //   // show element when show is true, hide when false
+  //   if (show) {
+  //     element.style.display = "inherit";
+  //   } else {
+  //     element.style.display = "none";
+  //   }
+  // }
+
+  function createListener(validator) {
+    return e => {
+      const text = e.target.value;
+      const valid = validator(text);
+      const showTip = text !== "" && !valid;
+      const tooltip = e.target.nextElementSibling;
+      showOrHideTip(showTip, tooltip);
+    };
+  }
+
+  $(nameInput).on("input", createListener(isValidName));
 });
